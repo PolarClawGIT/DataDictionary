1e552adfed5a4804552066b1989560ab796dd4b3
Billy <Billy@Prime.MyComputer>
2023-06-12 09:12:51 -0700

    Added Images for Navigation Tree.

--- DataDictionary.Main/Control/MainNavigation.cs
+++ DataDictionary.Main/Control/MainNavigation.cs
@@ -1,5 +1,6 @@
 ﻿using DataDictionary.BusinessLayer;
 using DataDictionary.DataLayer.DbMetaData;
+using DataDictionary.Main.Properties;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,6 +15,21 @@ namespace DataDictionary.Main.Control
 {
     public partial class MainNavigation : UserControl
     {
+        enum DbSchmeaTreeNodeType
+        {
+            Schema,
+            Table,
+            Column,
+            ColumnKey
+        }
+
+        readonly Dictionary<DbSchmeaTreeNodeType, Image> dbSchmeaTreeImages = new Dictionary<DbSchmeaTreeNodeType, Image>()
+        {
+            { DbSchmeaTreeNodeType.Schema, Resources.Schema },
+            { DbSchmeaTreeNodeType.Table, Resources.Table },
+            { DbSchmeaTreeNodeType.Column, Resources.Column },
+        };
+
         public MainNavigation()
         {
             InitializeComponent();
@@ -21,7 +37,12 @@ namespace DataDictionary.Main.Control
 
         private void MainNavigation_Load(object sender, EventArgs e)
         {
+            // Add Image Index to Db Schema Tree
+            if (dbSchmeaTreeData.ImageList is ImageList) { dbSchmeaTreeData.ImageList.Images.Clear(); }
+            else { dbSchmeaTreeData.ImageList = new ImageList(); }
 
+            foreach (DbSchmeaTreeNodeType item in Enum.GetValues(typeof(DbSchmeaTreeNodeType)))
+            { dbSchmeaTreeData.ImageList.Images.Add(item.ToString(), dbSchmeaTreeImages[item]); }
         }
 
         public void Bind(DataRepository data)
@@ -31,13 +52,17 @@ namespace DataDictionary.Main.Control
             databaseNameData.DataBindings.Clear();
             dbSchmeaTreeData.Nodes.Clear();
 
+
             // Bind the data
             serverNameData.DataBindings.Add(new Binding(nameof(serverNameData.Text), data, nameof(data.ServerName)));
             databaseNameData.DataBindings.Add(new Binding(nameof(databaseNameData.Text), data, nameof(data.DatabaseName)));
 
+
+            // Build Db Schema Tree Nodes
             foreach (IDbSchemaItem schemaItem in data.DbSchema.OrderBy(o => o.SchemaName))
             {
                 TreeNode schemaNode = new TreeNode(schemaItem.SchemaName);
+                schemaNode.ImageKey = DbSchmeaTreeNodeType.Schema.ToString() ;
                 dbSchmeaTreeData.Nodes.Add(schemaNode);
 
                 foreach (IDbTableItem tableItem in data.DbTable.
@@ -47,6 +72,7 @@ namespace DataDictionary.Main.Control
                     OrderBy(o => o.TableName))
                 {
                     TreeNode tableNode = new TreeNode(tableItem.TableName);
+                    tableNode.ImageKey = DbSchmeaTreeNodeType.Table.ToString();
                     schemaNode.Nodes.Add(tableNode);
 
                     foreach (IDbColumnItem columnItem in data.DbColumn.
@@ -56,6 +82,7 @@ namespace DataDictionary.Main.Control
                         OrderBy(o => o.OrdinalPosition))
                     {
                         TreeNode columnNode = new TreeNode(columnItem.ColumnName);
+                        columnNode.ImageKey = DbSchmeaTreeNodeType.Column.ToString();
                         tableNode.Nodes.Add(columnNode);
                     }
                 }
--- DataDictionary.Main/Properties/Resources.Designer.cs
+++ DataDictionary.Main/Properties/Resources.Designer.cs
@@ -61,12 +61,142 @@ namespace DataDictionary.Main.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Attribute {
+            get {
+                object obj = ResourceManager.GetObject("Attribute", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Column {
+            get {
+                object obj = ResourceManager.GetObject("Column", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
         internal static System.Drawing.Icon DataDictionaryApplication {
             get {
                 object obj = ResourceManager.GetObject("DataDictionaryApplication", resourceCulture);
                 return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Dictionary {
+            get {
+                object obj = ResourceManager.GetObject("Dictionary", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap ForeignKey {
+            get {
+                object obj = ResourceManager.GetObject("ForeignKey", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap ForeignKeyRelationship {
+            get {
+                object obj = ResourceManager.GetObject("ForeignKeyRelationship", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Key {
+            get {
+                object obj = ResourceManager.GetObject("Key", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap KeyColumn {
+            get {
+                object obj = ResourceManager.GetObject("KeyColumn", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Parameter {
+            get {
+                object obj = ResourceManager.GetObject("Parameter", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Procedure {
+            get {
+                object obj = ResourceManager.GetObject("Procedure", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Schema {
+            get {
+                object obj = ResourceManager.GetObject("Schema", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap StoredProcedure {
+            get {
+                object obj = ResourceManager.GetObject("StoredProcedure", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Table {
+            get {
+                object obj = ResourceManager.GetObject("Table", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap View {
+            get {
+                object obj = ResourceManager.GetObject("View", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
