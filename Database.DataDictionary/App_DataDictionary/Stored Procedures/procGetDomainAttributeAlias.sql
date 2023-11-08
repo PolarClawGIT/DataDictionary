b4f3f5bdd9ee41fca564babc5cf2cd327905bdea
Billy <Billy@Prime.MyComputer>
2023-11-08 07:16:59 -0800

    Fix how the Get of Alias Names from Attributes and Entities.

--- Database.DataDictionary/App_DataDictionary/Stored Procedures/procGetDomainAttributeAlias.sql
+++ Database.DataDictionary/App_DataDictionary/Stored Procedures/procGetDomainAttributeAlias.sql
@@ -6,16 +6,33 @@ Set NoCount On -- Do not show record counts
 Set XACT_ABORT On -- Error severity of 11 and above causes XAct_State() = -1 and a rollback must be issued
 /* Description: Performs Get on DomainAttributeAlias.
 */
+;With [Data] As (
+	Select	D.[AttributeId],
+			A.[AliasId],
+			D.[ScopeId],
+			A.[AliasParentId],
+			Convert(NVarChar(Max),FormatMessage('[%s]',[AliasElementName])) As [AliasName]
+	From	[App_DataDictionary].[DomainAttributeAlias] D
+			Inner Join [App_DataDictionary].[DomainAlias] A
+			On	D.[AliasId] = A.[AliasId]
+			Left Join [App_DataDictionary].[ModelAttribute] A
+			On	D.[AttributeId] = A.[AttributeId]
+	Where	(@ModelId is Null or @ModelId = A.[ModelId]) And
+			(@AttributeId is Null or @AttributeId = D.[AttributeId])
+	Union All
+	Select	D.[AttributeId],
+			D.[AliasId],
+			D.[ScopeId],
+			A.[AliasParentId],
+			Convert(NVarChar(Max),FormatMessage('[%s].%s',A.[AliasElementName],N.[AliasName])) As [AliasName]
+	From	[Data] D
+			Inner Join [App_DataDictionary].[DomainAlias] A
+			On	D.[AliasParentId] = A.[AliasId])
 Select	D.[AttributeId],
-		N.[AliasName],
+		D.[AliasName],
 		S.[ScopeName]
-From	[App_DataDictionary].[DomainAttributeAlias] D
-		Inner Join [App_DataDictionary].[ModelNameSpace] N
-		On	D.[AliasId] = N.[AliasId]
+From	[Data] D
 		Left Join [App_DataDictionary].[ModelScope] S
 		On	D.[ScopeId] = S.[ScopeId]
-		Left Join [App_DataDictionary].[ModelAttribute] A
-		On	D.[AttributeId] = A.[AttributeId]
-Where	(@ModelId is Null or @ModelId = A.[ModelId]) And
-		(@AttributeId is Null or @AttributeId = D.[AttributeId])
+Where	D.[AliasParentId] is Null
 GO
--- Database.DataDictionary/App_DataDictionary/Stored Procedures/procGetDomainEntityAlias.sql
+++ Database.DataDictionary/App_DataDictionary/Stored Procedures/procGetDomainEntityAlias.sql
@@ -6,16 +6,33 @@ Set NoCount On -- Do not show record counts
 Set XACT_ABORT On -- Error severity of 11 and above causes XAct_State() = -1 and a rollback must be issued
 /* Description: Performs Get on DomainEntityAlias.
 */
+;With [Data] As (
+	Select	D.[EntityId],
+			A.[AliasId],
+			D.[ScopeId],
+			A.[AliasParentId],
+			Convert(NVarChar(Max),FormatMessage('[%s]',[AliasElementName])) As [AliasName]
+	From	[App_DataDictionary].[DomainEntityAlias] D
+			Inner Join [App_DataDictionary].[DomainAlias] A
+			On	D.[AliasId] = A.[AliasId]
+			Left Join [App_DataDictionary].[ModelEntity] A
+			On	D.[EntityId] = A.[EntityId]
+	Where	(@ModelId is Null or @ModelId = A.[ModelId]) And
+			(@EntityId is Null or @EntityId = D.[EntityId])
+	Union All
+	Select	D.[EntityId],
+			D.[AliasId],
+			D.[ScopeId],
+			A.[AliasParentId],
+			Convert(NVarChar(Max),FormatMessage('[%s].%s',A.[AliasElementName],N.[AliasName])) As [AliasName]
+	From	[Data] D
+			Inner Join [App_DataDictionary].[DomainAlias] A
+			On	D.[AliasParentId] = A.[AliasId])
 Select	D.[EntityId],
-		N.[AliasName],
+		D.[AliasName],
 		S.[ScopeName]
-From	[App_DataDictionary].[DomainEntityAlias] D
-		Inner Join [App_DataDictionary].[ModelNameSpace] N
-		On	D.[AliasId] = N.[AliasId]
+From	[Data] D
 		Left Join [App_DataDictionary].[ModelScope] S
 		On	D.[ScopeId] = S.[ScopeId]
-		Left Join [App_DataDictionary].[ModelEntity] A
-		On	D.[EntityId] = A.[EntityId]
-Where	(@ModelId is Null or @ModelId = A.[ModelId]) And
-		(@EntityId is Null or @EntityId = D.[EntityId])
+Where	D.[AliasParentId] is Null
 GO
