98331a06e86707c3c9d22848a0be662e0baec544
Billy <Billy@Prime.MyComputer>
2024-01-01 13:04:46 -0800

    Discovered that T4 templates need to be manual run to be updated.

--- DataDictionary.BusinessLayer/WorkFlows/ModelFileExtension.cs
+++ DataDictionary.BusinessLayer/WorkFlows/ModelFileExtension.cs
@@ -15,47 +15,6 @@ namespace DataDictionary.BusinessLayer.WorkFlows
     /// </summary>
     public static class ModelFileExtension
     {
-        /// <summary>
-        /// WorkFlow to Saves the Model to the File
-        /// </summary>
-        /// <param name="data"></param>
-        /// <param name="file"></param>
-        /// <returns></returns>
-        public static IReadOnlyList<WorkItem> SaveModel(this ModelData data, FileInfo file)
-        { //TODO: Save to File System
-            data.ModelFile = file;
-
-            using (DataSet workset = new DataSet())
-            {
-                // repeat for each data object
-                using (DataTable model = new DataTable())
-                { }
-
-                workset.WriteXml(file.FullName, XmlWriteMode.WriteSchema); // Background work
-            }
-
-            return new List<WorkItem>().AsReadOnly();
-        }
-
-        /// <summary>
-        /// WorkFlow to Loads a Model from a File
-        /// </summary>
-        /// <param name="data"></param>
-        /// <param name="file"></param>
-        /// <returns></returns>
-        public static IReadOnlyList<WorkItem> LoadModel(this ModelData data, FileInfo file)
-        { //TODO: Load from File System
-
-            using (DataSet workset = new DataSet())
-            {
-                workset.ReadXml(file.FullName, XmlReadMode.ReadSchema); // background work
-
-                // Repeat for each data object, background work
-                if (workset.Tables.Contains(nameof(ModelItem)) && workset.Tables[nameof(ModelItem)] is DataTable source)
-                {
-                }
-            }
-            return new List<WorkItem>().AsReadOnly();
-        }
+  
     }
 }
