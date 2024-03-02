da4b8c53e5963be2f519d32af00b3e0fcefbb8ed
Billy <Billy@Prime.MyComputer>
2024-03-02 07:57:50 -0800

    Changed Version Numbering to be consistent with the branching structure.  Year and Month instead of Year and Day.

--- DataDictionary.DataLayer/VersionNumber.cs
+++ DataDictionary.DataLayer/VersionNumber.cs
@@ -4,5 +4,5 @@
 // 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.2024.38")]
-[assembly: AssemblyFileVersion("1.0.2024.38")]
+[assembly: AssemblyVersion("1.0.2024.3")]
+[assembly: AssemblyFileVersion("1.0.2024.3")]
