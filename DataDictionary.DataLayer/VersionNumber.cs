1d6cb3f84020ea0619fd6e6d548a9d43baa32d3d
Billy <Billy@Prime.MyComputer>
2024-04-01 07:25:05 -0700

    Updated Version Number.

--- DataDictionary.DataLayer/VersionNumber.cs
+++ DataDictionary.DataLayer/VersionNumber.cs
@@ -4,5 +4,5 @@
 // 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.2024.3")]
-[assembly: AssemblyFileVersion("1.0.2024.3")]
+[assembly: AssemblyVersion("1.0.2024.4")]
+[assembly: AssemblyFileVersion("1.0.2024.4")]
