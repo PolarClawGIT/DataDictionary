fc54abb5eefd8ab70dc5d89ec3fdcdbafa1ee894
Billy <Billy@Prime.MyComputer>
2024-06-10 08:11:40 -0700

    Remove old Scripting tables from the Database.

--- Database.DataDictionary/Obsolete/typeScriptingTransform.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TYPE [App_DataDictionary].[typeScriptingTransform] AS TABLE
-(
-	[TransformId]             UniqueIdentifier NULL,
-	[TransformTitle]          [App_DataDictionary].[typeTitle] Null,
-	[TransformDescription]    [App_DataDictionary].[typeDescription] Null,
-	[AsText]                  Bit Null,
-	[AsXml]                   Bit Null,
-	[TransformScript]         NVarChar(Max) Null -- C# code will use String and convert to XElement
-)
