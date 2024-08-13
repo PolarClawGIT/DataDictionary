8c88e9ce3a888eeb36258bfe40f149b45d7a6ff4
Billy <Billy@Prime.MyComputer>
2024-08-13 10:52:55 -0700

    Added additional PF/FK scenario's to sample database to test against.

---
+++ DataDictionary.SampleDatabase/dbo/Tables/SampleDouble.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SampleDouble]
+(
+    [SampleParentId]       INT NOT NULL,
+	[SampleAliasId]        INT NOT Null,
+	[SampleDoubleData]     XML NOT NULL,
+    CONSTRAINT [PK_SampleDouble] PRIMARY KEY CLUSTERED ([SampleParentId] ASC, [SampleAliasId] ASC),
+	CONSTRAINT [FK_SampleDouble_SampleParent] FOREIGN KEY ([SampleParentId]) REFERENCES [dbo].[SampleParent] ([SampleParentId]),
+	CONSTRAINT [FK_SampleDoubleAlt_SampleParent] FOREIGN KEY ([SampleAliasId]) REFERENCES [dbo].[SampleParent] ([SampleParentId])
+)
---
+++ DataDictionary.SampleDatabase/dbo/Tables/SampleTree.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[SampleTree]
+(
+	[SampleTreeId]       INT NOT NULL,
+	[SampleTreeParentId] INT NULL,
+	[SampleTreeData]     XML NOT NULL,
+	CONSTRAINT [PK_SampleTree] PRIMARY KEY CLUSTERED ([SampleTreeId] ASC),
+	CONSTRAINT [FK_SampleTree_Parent] FOREIGN KEY ([SampleTreeParentId]) REFERENCES [dbo].[SampleTree] ([SampleTreeId]),
+)
