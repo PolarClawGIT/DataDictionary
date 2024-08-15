e938f55f06bae56f61d1e67a309b4e4984556780
Billy <Billy@Prime.MyComputer>
2024-08-15 10:35:49 -0700

    Added MS Description into Sample db for testing.

--- DataDictionary.SampleDatabase/dbo/Tables/SampleChild.sql
+++ DataDictionary.SampleDatabase/dbo/Tables/SampleChild.sql
@@ -5,3 +5,22 @@
     CONSTRAINT [PK_SampleChild] PRIMARY KEY CLUSTERED ([SampleParentId] ASC, [SampleChildId] ASC),
     CONSTRAINT [FK_SampleChild_SampleParent] FOREIGN KEY ([SampleParentId]) REFERENCES [dbo].[SampleParent] ([SampleParentId])
 );
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Sample Child Table Description',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'SampleChild',
+    @level2type = NULL,
+    @level2name = NULL
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Child Table Primary Key Refrence',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'SampleChild',
+    @level2type = N'COLUMN',
+    @level2name = N'SampleParentId'
--- DataDictionary.SampleDatabase/dbo/Tables/SampleParent.sql
+++ DataDictionary.SampleDatabase/dbo/Tables/SampleParent.sql
@@ -12,3 +12,20 @@ GO
 CREATE NONCLUSTERED INDEX [IX_SampleParent]
     ON [dbo].[SampleParent]([SampleSequence] ASC);
 GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Primary Key Column of the Sample Parent Table',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'SampleParent',
+    @level2type = N'COLUMN',
+    @level2name = N'SampleParentId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Sample Parent Table Description',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'SampleParent',
+    @level2type = NULL,
+    @level2name = NULL
