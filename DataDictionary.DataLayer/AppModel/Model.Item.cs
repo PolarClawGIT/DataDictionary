a9a62b2e0ad46ac3c7ce51ddc49bd7f96ae28df4
Billy <Billy@Prime.MyComputer>
2024-12-11 07:04:28 -0800

    Trying a different way of doing common code for Temporal Data.

--- DataDictionary.DataLayer/AppModel/Model.Item.cs
+++ DataDictionary.DataLayer/AppModel/Model.Item.cs
@@ -18,6 +18,8 @@ namespace DataDictionary.DataLayer.AppModel
     [Serializable]
     public class ModelItem : BindingTableRow, IModelItem, ISerializable
     {
+        TemporalItem temporal; // Backing field for Temporal Data.
+
         /// <inheritdoc/>
         public Guid? ModelId { get { return GetValue<Guid>(nameof(ModelId)); } protected set { SetValue(nameof(ModelId), value); } }
 
@@ -27,64 +29,32 @@ namespace DataDictionary.DataLayer.AppModel
         /// <inheritdoc/>
         public String? ModelDescription { get { return GetValue(nameof(ModelDescription)); } set { SetValue(nameof(ModelDescription), value); } }
 
+        /// <inheritdoc/>
+        public DateTime? CreatedOn { get { return temporal.CreatedOn; } }
 
         /// <inheritdoc/>
-        public String? CreatedBy { get { return GetValue(nameof(CreatedBy)); } }
+        public String? CreatedBy { get { return temporal.CreatedBy; } }
 
         /// <inheritdoc/>
-        public DateTime? CreatedOn
-        {
-            get
-            {
-                DateTime? value = GetValue<DateTime>(nameof(CreatedOn));
-                if (value is DateTime baseDate)
-                { return TimeZoneInfo.ConvertTimeFromUtc(baseDate, TimeZoneInfo.Local); }
-                else { return null; }
-            }
-        }
+        public DateTime? RemovedOn { get { return temporal.RemovedOn; } }
 
         /// <inheritdoc/>
-        public String? RemovedBy { get { return GetValue(nameof(RemovedBy)); } }
+        public String? RemovedBy { get { return temporal.RemovedBy; } }
 
         /// <inheritdoc/>
-        public DateTime? RemovedOn
-        {
-            get
-            {
-                DateTime? value = GetValue<DateTime>(nameof(RemovedOn));
-                if (value is DateTime baseDate)
-                { return TimeZoneInfo.ConvertTimeFromUtc(baseDate, TimeZoneInfo.Local); }
-                else { return null; }
-            }
-        }
+        public Boolean? IsInserted { get { return temporal.IsInserted; } }
 
         /// <inheritdoc/>
-        public Boolean? IsInserted
-        { get { return GetValue<Boolean>(nameof(IsInserted), BindingItemParsers.BooleanTryParse); } }
+        public Boolean? IsUpdated { get { return temporal.IsUpdated; } }
 
         /// <inheritdoc/>
-        public Boolean? IsUpdated
-        { get { return GetValue<Boolean>(nameof(IsUpdated), BindingItemParsers.BooleanTryParse); } }
+        public Boolean? IsDeleted { get { return temporal.IsDeleted; } }
 
         /// <inheritdoc/>
-        public Boolean? IsDeleted
-        { get { return GetValue<Boolean>(nameof(IsDeleted), BindingItemParsers.BooleanTryParse); } }
+        public Boolean? IsCurrent { get { return temporal.IsCurrent; } }
 
         /// <inheritdoc/>
-        public Boolean? IsCurrent
-        { get { return GetValue<Boolean>(nameof(IsCurrent), BindingItemParsers.BooleanTryParse); } }
-
-        /// <inheritdoc/>
-        public DbModificationType Modification
-        {
-            get
-            {
-                if (IsDeleted == true) { return DbModificationType.Deleted; }
-                else if (IsInserted == true) { return DbModificationType.Inserted; }
-                else if (IsUpdated == true) { return DbModificationType.Updated; }
-                else { return DbModificationType.Null; }
-            }
-        }
+        public DbModificationType Modification { get { return temporal.Modification; } }
 
         /// <summary>
         /// Constructor for the Model data
@@ -93,22 +63,21 @@ namespace DataDictionary.DataLayer.AppModel
         {
             ModelId = Guid.NewGuid();
             ModelTitle = "New Model";
+            temporal = new TemporalItem()
+            {
+                GetBoolean = (name) => GetValue<Boolean>(name, BindingItemParsers.BooleanTryParse),
+                GetDate = GetValue<DateTime>,
+                GetString = GetValue,
+            };
         }
 
-        static readonly IReadOnlyList<DataColumn> columnDefinitions = new List<DataColumn>()
-        {
-            new DataColumn(nameof(ModelId), typeof(Guid)){ AllowDBNull = false},
-            new DataColumn(nameof(ModelTitle), typeof(string)){ AllowDBNull = false},
-            new DataColumn(nameof(ModelDescription), typeof(string)){ AllowDBNull = true},
-            new DataColumn(nameof(CreatedOn), typeof(DateTime)){ AllowDBNull = true},
-            new DataColumn(nameof(CreatedBy), typeof(String)){ AllowDBNull = true},
-            new DataColumn(nameof(RemovedOn), typeof(DateTime)){ AllowDBNull = true},
-            new DataColumn(nameof(RemovedBy), typeof(String)){ AllowDBNull = true},
-            new DataColumn(nameof(IsInserted), typeof(Boolean)){ AllowDBNull = true},
-            new DataColumn(nameof(IsUpdated), typeof(Boolean)){ AllowDBNull = true},
-            new DataColumn(nameof(IsDeleted), typeof(Boolean)){ AllowDBNull = true},
-            new DataColumn(nameof(IsCurrent), typeof(Boolean)){ AllowDBNull = true},
-        };
+        static readonly IReadOnlyList<DataColumn> columnDefinitions =
+                [
+                    new DataColumn(nameof(ModelId), typeof(Guid)) { AllowDBNull = false},
+                    new DataColumn(nameof(ModelTitle), typeof(string)) { AllowDBNull = false},
+                    new DataColumn(nameof(ModelDescription), typeof(string)) { AllowDBNull = true},
+                    .. TemporalItem.columnDefinitions,
+                ];
 
         /// <inheritdoc/>
         public override IReadOnlyList<DataColumn> ColumnDefinitions()
@@ -135,7 +104,14 @@ namespace DataDictionary.DataLayer.AppModel
         /// <param name="serializationInfo"></param>
         /// <param name="streamingContext"></param>
         protected ModelItem(SerializationInfo serializationInfo, StreamingContext streamingContext) : base(serializationInfo, streamingContext)
-        { }
+        {
+            temporal = new TemporalItem()
+            {
+                GetBoolean = (name) => GetValue<Boolean>(name, BindingItemParsers.BooleanTryParse),
+                GetDate = GetValue<DateTime>,
+                GetString = GetValue,
+            };
+        }
         #endregion
     }
 }
--- DataDictionary.DataLayer/Temporal.Item.cs
+++ DataDictionary.DataLayer/Temporal.Item.cs
@@ -1,11 +1,13 @@
 ﻿using DataDictionary.Resource.Enumerations;
+using System.Data;
+using Toolbox.BindingTable;
 
 namespace DataDictionary.DataLayer;
 
 /// <summary>
 /// Interface describes a Temporal Table
 /// </summary>
-public interface ITemporalItem: ITemporalKey
+public interface ITemporalItem : ITemporalKey
 {
     /// <summary>
     /// Account Name that Created (Insert/Update) this record
@@ -46,4 +48,95 @@ public interface ITemporalItem: ITemporalKey
     /// Type of Modification made.
     /// </summary>
     DbModificationType Modification { get; }
+}
+
+/// <summary>
+/// Temporal sub-class for common functionality of Temporal data.
+/// </summary>
+class TemporalItem: ITemporalItem
+{
+    /// <inheritdoc cref="BindingTableRow.GetValue(string)"/>
+    /// <remarks>Pass: BindingTableRow.GetValue</remarks>
+    public required Func<String,String?> GetString { get; init; }
+
+    /// <inheritdoc cref="BindingTableRow.GetValue{T}(string)"/>
+    /// <remarks><![CDATA[Pass: BindingTableRow.GetValue<DateTime>]]></remarks>
+    public required Func<String, DateTime?> GetDate { get; init; }
+
+    /// <inheritdoc cref="BindingTableRow.GetValue{T}(string, BindingTableRow.tryParseDelegate{T})"/>
+    /// <remarks><![CDATA[Pass: BindingTableRow.GetValue<Boolean>(name, BindingItemParsers.BooleanTryParse)]]></remarks>
+    public required Func<String, Boolean?> GetBoolean { get; init; }
+
+    /// <inheritdoc/>
+    public String? CreatedBy { get { return GetString(nameof(CreatedBy)); } }
+
+    /// <inheritdoc/>
+    public DateTime? CreatedOn
+    {
+        get
+        {
+            DateTime? value = GetDate(nameof(CreatedOn));
+            if (value is DateTime baseDate)
+            { return TimeZoneInfo.ConvertTimeFromUtc(baseDate, TimeZoneInfo.Local); }
+            else { return null; }
+        }
+    }
+
+    /// <inheritdoc/>
+    public String? RemovedBy { get { return GetString(nameof(RemovedBy)); } }
+
+    /// <inheritdoc/>
+    public DateTime? RemovedOn
+    {
+        get
+        {
+            DateTime? value = GetDate(nameof(RemovedOn));
+            if (value is DateTime baseDate)
+            { return TimeZoneInfo.ConvertTimeFromUtc(baseDate, TimeZoneInfo.Local); }
+            else { return null; }
+        }
+    }
+
+    /// <inheritdoc/>
+    public Boolean? IsInserted
+    { get { return GetBoolean(nameof(IsInserted)); } }
+
+    /// <inheritdoc/>
+    public Boolean? IsUpdated
+    { get { return GetBoolean(nameof(IsUpdated)); } }
+
+    /// <inheritdoc/>
+    public Boolean? IsDeleted
+    { get { return GetBoolean(nameof(IsDeleted)); } }
+
+    /// <inheritdoc/>
+    public Boolean? IsCurrent
+    { get { return GetBoolean(nameof(IsCurrent)); } }
+
+    /// <inheritdoc/>
+    public DbModificationType Modification
+    {
+        get
+        {
+            if (IsDeleted == true) { return DbModificationType.Deleted; }
+            else if (IsInserted == true) { return DbModificationType.Inserted; }
+            else if (IsUpdated == true) { return DbModificationType.Updated; }
+            else { return DbModificationType.Null; }
+        }
+    }
+
+    /// <summary>
+    /// List of Temporal Columns
+    /// </summary>
+    public static readonly IReadOnlyList<DataColumn> columnDefinitions =
+        [
+            new DataColumn(nameof(CreatedOn), typeof(DateTime)){ AllowDBNull = true},
+            new DataColumn(nameof(CreatedBy), typeof(String)){ AllowDBNull = true},
+            new DataColumn(nameof(RemovedOn), typeof(DateTime)){ AllowDBNull = true},
+            new DataColumn(nameof(RemovedBy), typeof(String)){ AllowDBNull = true},
+            new DataColumn(nameof(IsInserted), typeof(Boolean)){ AllowDBNull = true},
+            new DataColumn(nameof(IsUpdated), typeof(Boolean)){ AllowDBNull = true},
+            new DataColumn(nameof(IsDeleted), typeof(Boolean)){ AllowDBNull = true},
+            new DataColumn(nameof(IsCurrent), typeof(Boolean)){ AllowDBNull = true},
+        ];
 }
