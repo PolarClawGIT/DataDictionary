544f007c7ef7fa66b414c2719a15a1c2e6ad8742
Billy <Billy@Prime.MyComputer>
2024-12-24 09:53:06 -0800

    Missed Model Entity when setting up temporal.

---
+++ Database.DataDictionary/HsModel/Tables/ModelEntity.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [HsModel].[ModelEntity] (
+    [ModelId]       UniqueIdentifier                                   NOT NULL,
+    [EntityId]      UniqueIdentifier                                   NOT NULL,
+    [SysStart]      DateTime2 (7)    NOT NULL,
+    [SysEnd]        DateTime2 (7)    NOT NULL
+)
+GO
+CREATE CLUSTERED INDEX [IX_ModelEntity]
+    ON [HsModel].[ModelEntity]([SysEnd] ASC, [SysStart] ASC)
+GO
+CREATE INDEX [FK_ModelEntity]
+    ON [HsModel].[ModelEntity]([ModelId] ASC, [EntityId] ASC)
+GO
