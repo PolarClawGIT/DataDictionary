d11c6dfdeec426c52e783d6ceec6db6e6c57b0e3
Billy <Billy@Prime.MyComputer>
2025-05-21 08:12:22 -0700

    Working Alias control. Needed changes to deal with Binding not working on complex types.

--- DataDictionary.Main/Forms/Model/Controls/Alias.cs
+++ DataDictionary.Main/Forms/Model/Controls/Alias.cs
@@ -23,6 +23,20 @@ namespace DataDictionary.Main.Forms.Model.Controls
         BindingSource? dataBinding; // Pointer to the BindingSource.
         List<ScopeType> filterScope = new List<ScopeType>();
 
+        //TODO: Testing, write to Entity and Attribute.
+
+        /// <summary>
+        /// List of Alias in the BindingSource.
+        /// </summary>
+        public IEnumerable<IAliasSubType> Aliases
+        {
+            get
+            {
+                if (dataBinding is null) { return new List<IAliasSubType>(); }
+                else { return dataBinding.List.OfType<IAliasSubType>(); }
+            }
+        }
+
         public Alias()
         {
             InitializeComponent();
@@ -60,14 +74,21 @@ namespace DataDictionary.Main.Forms.Model.Controls
             {
                 if (dataBinding.Current is IAliasSubType current)
                 {
+                    aliasNameData.Text = current.AliasPath.MemberFullPath;
                     Boolean inModel = BusinessData.NamedScope.PathKeys(current.AliasPath).Count > 0;
                     isAliasInModelData.Checked = inModel;
                     aliasNameData.ReadOnly = inModel;
                     aliasScopeData.ReadOnly = inModel;
                 }
+                else
+                {
+                    aliasNameData.Text = String.Empty;
+                    isAliasInModelData.Checked = false;
+                    aliasNameData.ReadOnly = true;
+                    aliasScopeData.ReadOnly = true;
+                }
             }
         }
-
 
         private void AliasNameData_Validating(object sender, CancelEventArgs e)
         {
@@ -89,23 +110,25 @@ namespace DataDictionary.Main.Forms.Model.Controls
         private void AliasSelectCommand_Click(object sender, EventArgs e)
         {
             if (dataBinding is not null
-                && ParentForm is not null
-                && dataBinding.List is IList<IAliasSubType> alias)
+                && ParentForm is not null)
             {
                 using (SelectionDialog dialog = new SelectionDialog(ParentForm))
                 {
                     dialog.FilterScopes.AddRange(filterScope);
-                    dialog.BuildData(alias.SelectMany(s => BusinessData.NamedScope.PathKeys(s.AliasPath)));
+                    dialog.BuildData(Aliases.SelectMany(s => BusinessData.NamedScope.PathKeys(s.AliasPath)));
 
                     if (dialog.ShowDialog(this) is DialogResult.OK)
                     {
                         foreach (INamedScopeValue item in dialog.SelectedByNamedScope())
                         {
-                            if (!alias.Any(w => w.AliasScope.Equals(item.Scope) && w.AliasPath.Equals(item.Path))
+                            if (!Aliases.Any(w => w.AliasScope.Equals(item.Scope) && w.AliasPath.Equals(item.Path))
                                 && dataBinding.AddNew() is IAliasSubType value)
                             {
                                 value.AliasScope = item.Scope;
                                 value.AliasPath = item.Path;
+
+                                dataBinding.Position = Aliases.ToList().IndexOf(value);
+                                aliasNameData.Text = value.AliasPath.MemberFullPath;
                             }
                         }
                     }
