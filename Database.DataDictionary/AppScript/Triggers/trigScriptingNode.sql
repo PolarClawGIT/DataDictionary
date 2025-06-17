fc585d0f314672fc3f798f9a525441e8b36feff5
Billy <Billy@Prime.MyComputer>
2025-06-17 13:51:08 -0700

    Triggers for Library and Scripting.

---
+++ Database.DataDictionary/AppScript/Triggers/trigScriptingNode.sql
@@ -0,0 +1,8 @@
+﻿CREATE TRIGGER [AppScript].[trigScriptingNode]
+	ON [AppScript].[ScriptingNode]
+	FOR DELETE, INSERT, UPDATE
+	AS
+	BEGIN
+	-- Set Transaction Log
+	Exec [AppGeneral].[procRecordTransactionLog] @ProcId = @@ProcId
+	END
