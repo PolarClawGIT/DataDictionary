fc953ad0024b9f12f4084d3080f040023209b685
Billy <Billy@Prime.MyComputer>
2025-07-23 08:22:15 -0700

    Add Get procedures for Scripting Data.

---
+++ Database.DataDictionary/AppScript/Stored Procedures/procGetDataItem.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE [AppScript].[procGetDataItem]
+		@ModelId UniqueIdentifier = Null,
+		@DataSourceId UniqueIdentifier = Null,
+		@AsOfUtcDate DateTime2 (7) = Null, -- As of this UTC Date (account for timezone offset). Default is now.
+		@IncludeHistory Bit = 0 -- History is included
+As
+Select	[DataSourceId],
+		[DataItemId],
+		[DataItemMember],
+		[DataNameSpace],
+		[HierarchyId],
+		-- Temporal Data
+		[CreatedOn],
+		[CreatedBy],
+		[RemovedOn], 
+		[RemovedBy],
+		[IsInserted],
+		[IsUpdated],
+		[IsDeleted],
+		[IsCurrent]
+From	[AppScript].[DataItemHs] D
+Where	(@IncludeHistory = 1 Or ([SysStart] <= @AsOfUtcDate And [SysEnd] > @AsOfUtcDate)) And
+		(@DataSourceId is Null Or @DataSourceId = [DataSourceId]) And
+		(@ModelId is Null Or @ModelId In (
+			Select	[ModelId]
+			From	[AppScript].[ScriptingModel] -- TODO: For System_Time As of @AsOfUtcDate
+			Where	D.[DataSourceId] = [DataSourceId]))
+Go
---
+++ Database.DataDictionary/AppScript/Stored Procedures/procGetTemplateData.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [AppScript].[procGetTemplateData]
+		@ModelId UniqueIdentifier = Null,
+		@TemplateId UniqueIdentifier = Null,
+		@DataSourceId UniqueIdentifier = Null,
+		@AsOfUtcDate DateTime2 (7) = Null, -- As of this UTC Date (account for timezone offset). Default is now.
+		@IncludeHistory Bit = 0 -- History is included
+As
+Select	[TemplateId],
+		[DataSourceId],
+		-- Temporal Data
+		[CreatedOn],
+		[CreatedBy],
+		[RemovedOn], 
+		[RemovedBy],
+		[IsInserted],
+		[IsUpdated],
+		[IsDeleted],
+		[IsCurrent]
+From	[AppScript].[TemplateDataHs] D
+Where	(@IncludeHistory = 1 Or ([SysStart] <= @AsOfUtcDate And [SysEnd] > @AsOfUtcDate)) And
+		(@TemplateId is Null Or @TemplateId = [TemplateId]) And
+		(@DataSourceId is Null Or @DataSourceId = [DataSourceId]) And
+		(@ModelId is Null Or @ModelId = [ModelId])
+Go
