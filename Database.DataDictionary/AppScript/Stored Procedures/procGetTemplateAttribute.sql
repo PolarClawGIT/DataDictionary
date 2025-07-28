92355f9e24553ee00bd8be8833a7bcaf853dd86f
PolarClawGit <PolarClawIce@Comcast.Net>
2025-07-28 08:30:01 -0700

    Remove TemplateId from Get Template Attribute to match table structure.

--- Database.DataDictionary/AppScript/Stored Procedures/procGetTemplateAttribute.sql
+++ Database.DataDictionary/AppScript/Stored Procedures/procGetTemplateAttribute.sql
@@ -11,9 +11,8 @@ Set XACT_ABORT On -- Error severity of 11 and above causes XAct_State() = -1 and
 */
 Set	@AsOfUtcDate = IsNull(@AsOfUtcDate, SysUtcDatetime())
 
-Select	[TemplateId],
+Select	[AttributeId],
 		[NodeId],
-		[AttributeId],
 		[AttributeName],
 		[AttributeValue],
 		[PropertyId],
--- Database.DataDictionary/AppScript/User Defined Table Types/udttTemplateAttribute.sql
+++ Database.DataDictionary/AppScript/User Defined Table Types/udttTemplateAttribute.sql
@@ -1,8 +1,7 @@
 ﻿CREATE TYPE [AppScript].[udttTemplateAttribute] AS TABLE
 (
-	[TemplateId]            UniqueIdentifier NULL,
-	[NodeId]	            UniqueIdentifier NULL,
 	[AttributeId]			UniqueIdentifier NULL,
+	[NodeId]	            UniqueIdentifier NULL,
 	[AttributeName]			NVarChar(50) NULL,
 	[AttributeValue]		NVarChar(250) NULL,
 	[PropertyId]			UniqueIdentifier NULL,
