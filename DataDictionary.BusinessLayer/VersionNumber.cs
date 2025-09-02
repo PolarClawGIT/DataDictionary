efaaf3a90200ba7ac76270821e644bcc7ac6112f
PolarClawGit <PolarClawIce@Comcast.Net>
2025-09-02 13:54:11 -0700

    Version Number updated.

--- DataDictionary.BusinessLayer/VersionNumber.cs
+++ DataDictionary.BusinessLayer/VersionNumber.cs
@@ -4,5 +4,5 @@
 // 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.2025.8")]
-[assembly: AssemblyFileVersion("1.0.2025.8")]
+[assembly: AssemblyVersion("1.0.2025.9")]
+[assembly: AssemblyFileVersion("1.0.2025.9")]
