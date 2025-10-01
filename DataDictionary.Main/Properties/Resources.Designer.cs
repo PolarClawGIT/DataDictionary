275b23909a1d73e420ad31e7018f5ef572fe46ff
PolarClawGit <PolarClawIce@Comcast.Net>
2025-10-01 07:03:58 -0700

    Matched image name to the Resoruce Name.

--- DataDictionary.Main/Properties/Resources.Designer.cs
+++ DataDictionary.Main/Properties/Resources.Designer.cs
@@ -953,26 +953,6 @@ namespace DataDictionary.Main.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap RefreshTree {
-            get {
-                object obj = ResourceManager.GetObject("RefreshTree", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap ReloadTree {
-            get {
-                object obj = ResourceManager.GetObject("ReloadTree", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap Save {
             get {
                 object obj = ResourceManager.GetObject("Save", resourceCulture);
