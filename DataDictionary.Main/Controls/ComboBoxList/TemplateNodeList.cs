5aee7a66a45494b4ddcd0e38f96b1172ed12a54a
PolarClawGit <PolarClawIce@Comcast.Net>
2025-10-30 09:52:17 -0700

    Building a Template Node List combobox helper class.
    This is adding new behavior that was not in other combobox helper classes. This looks for changes in the source bindinglist and refelects them into the combobox list. Considering doing the same thing in others.

---
+++ DataDictionary.Main/Controls/ComboBoxList/TemplateNodeList.cs
@@ -0,0 +1,149 @@
+﻿using DataDictionary.BusinessLayer.AppScripting;
+using DataDictionary.Resource;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataDictionary.Main.Controls.ComboBoxList
+{
+    record TemplateNodeList : ITemplateNodeIndex, ITemplateNodeIndexName, INotifyPropertyChanged
+    {
+        /// <inheritdoc/>
+        public Guid? NodeId { get; private set; } = Guid.Empty;
+
+        /// <inheritdoc/>
+        public String NodeName { get; private set; } = String.Empty;
+
+        TemplateNodeList()
+        { NodeName = "(n/a)"; }
+
+        TemplateNodeList(ITemplateNodeValue value)
+        {
+            NodeId = value.NodeId;
+            NodeName = value.NodeName ?? String.Empty;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        void OnChanged(ITemplateNodeValue value)
+        {
+            NodeName = value.NodeName ?? String.Empty;
+
+            if (PropertyChanged is PropertyChangedEventHandler handler)
+            { handler(this, new PropertyChangedEventArgs(nameof(NodeName))); }
+        }
+
+        public static void Load(DataGridViewComboBoxColumn control, ITemplateIndex template)
+        {
+            control.ValueMember = nameof(NodeId);
+            control.DisplayMember = nameof(NodeName);
+            control.DataSource = BuildList(template);
+        }
+
+        public static void Load(ComboBoxData control, ITemplateIndex template)
+        {
+            control.ValueMember = nameof(NodeId);
+            control.DisplayMember = nameof(NodeName);
+            control.DataSource = BuildList(template);
+        }
+
+        static BindingList<TemplateNodeList> BuildList(ITemplateIndex template)
+        {
+            TemplateIndex templateKey = new TemplateIndex(template);
+
+            BindingList<TemplateNodeList> comboList = new BindingList<TemplateNodeList>();
+            comboList.Add(new TemplateNodeList());
+
+            comboList.AddRange(
+                BusinessData.Scripting.Nodes.
+                Where(w => templateKey.Equals(w)).
+                OrderBy(o => o.NodeName).
+                Select( s=> new TemplateNodeList(s)));
+
+            BusinessData.Scripting.Nodes.ListChanged += Nodes_ListChanged;
+            return comboList;
+
+            void Nodes_ListChanged(Object? sender, ListChangedEventArgs e)
+            {
+                switch (e.ListChangedType)
+                {
+                    case ListChangedType.Reset:
+                        comboList.RaiseListChangedEvents = false;
+
+                        comboList.RemoveRange(comboList.
+                            Where(w => w.NodeId != Guid.Empty));
+
+                        comboList.AddRange(
+                            BusinessData.Scripting.Nodes.
+                            Where(w => templateKey.Equals(w)).
+                            OrderBy(o => o.NodeName).
+                            Select(s => new TemplateNodeList(s)));
+
+                        comboList.RaiseListChangedEvents = true;
+                        comboList.ResetBindings();
+
+                        break;
+                    case ListChangedType.ItemAdded:
+                        var newItem = BusinessData.Scripting.Nodes[e.NewIndex];
+                        var newIndex = comboList.Count;
+
+                        if (comboList.LastOrDefault(w => String.Compare(w.NodeName, newItem.NodeName ?? String.Empty, true) < 0) is TemplateNodeList priorItem)
+                        { newIndex = comboList.IndexOf(priorItem); }
+
+                        if (templateKey.Equals(newItem)
+                            && !comboList.Any(w => new TemplateNodeIndex(newItem).Equals(w)))
+                        { comboList.Insert(newIndex, new TemplateNodeList(newItem)); }
+
+                        break;
+                    case ListChangedType.ItemDeleted:
+                        comboList.RemoveRange(comboList.
+                            Where(w => comboList.
+                                Where(w => w.NodeId != Guid.Empty).
+                                Select(s => new TemplateNodeIndex(s)).
+                                Except(BusinessData.Scripting.Nodes.Where(w => templateKey.Equals(w)).
+                                    Select(s => new TemplateNodeIndex(s))).
+                                Any(a => a.Equals(w))));
+                        break;
+                    case ListChangedType.ItemMoved:
+                        break;
+                    case ListChangedType.ItemChanged:
+                        var changedItem = BusinessData.Scripting.Nodes[e.NewIndex];
+                        var changedKey = new TemplateNodeIndex(changedItem);
+                        var changedIndex = comboList.Count;
+                        
+                        if (templateKey.Equals(changedItem))
+                        {
+                            comboList.RaiseListChangedEvents = false;
+
+                            comboList.RemoveRange(w => changedKey.Equals(w));
+
+                            if (comboList.LastOrDefault(w => String.Compare(w.NodeName, changedItem.NodeName ?? String.Empty, true) < 0) is TemplateNodeList priorChangedItem)
+                            { changedIndex = comboList.IndexOf(priorChangedItem); }
+
+                            if (templateKey.Equals(changedItem)
+                                && !comboList.Any(w => new TemplateNodeIndex(changedItem).Equals(w)))
+                            { comboList.Insert(changedIndex, new TemplateNodeList(changedItem)); }
+
+                            comboList.RaiseListChangedEvents = true;
+                            comboList.ResetBindings();
+
+
+                            //TODO: Discover if the combobox control will reset to the choosen item or stick to the index of the item.
+                            // can the binding list Move event be fired? Would that handle this scenario?
+                        }
+                        break;
+                    case ListChangedType.PropertyDescriptorAdded:
+                    case ListChangedType.PropertyDescriptorDeleted:
+                    case ListChangedType.PropertyDescriptorChanged:
+                    default:
+                        break;
+                }
+
+
+            }
+        }
+    }
+}
--- DataDictionary.Resource/CollectionExtension.cs
+++ DataDictionary.Resource/CollectionExtension.cs
@@ -17,5 +17,29 @@
             foreach (T item in source.ToList())
             { target.Add(item); }
         }
+
+        /// <summary>
+        /// Removes specfic elements of the given collection.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="target"></param>
+        /// <param name="source"></param>
+        public static void RemoveRange<T>(this ICollection<T> target, IEnumerable<T> source)
+        {
+            foreach (T item in source.ToList())
+            { target.Remove(item); }
+        }
+
+        /// <summary>
+        /// Removes elements that meet the condition of the given collection.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="target"></param>
+        /// <param name="filter"></param>
+        public static void RemoveRange<T>(this ICollection<T> target, Func<T,Boolean> filter)
+        {
+            foreach (T item in target.Where(w => filter(w)).ToList())
+            { target.Remove(item); }
+        }
     }
 }
