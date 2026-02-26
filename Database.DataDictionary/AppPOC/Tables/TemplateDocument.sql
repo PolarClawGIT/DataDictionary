b306df1260b5cba06c4bb8dd68357f67d193bbb9
PolarClawGit <PolarClawIce@Comcast.Net>
2026-02-26 07:36:19 -0800

    Allowed Template Document to be "fixed" where the input or output is not determined by Object.

--- Database.DataDictionary/AppPOC/Tables/TemplateDocument.sql
+++ Database.DataDictionary/AppPOC/Tables/TemplateDocument.sql
@@ -2,11 +2,13 @@
 (	-- Supertype: XSLT Input & Output
 	[TemplateId]            UniqueIdentifier Not Null,
 	[DocumentId]            UniqueIdentifier Not Null,
+	-- all Null: (Input, fixed)
 	-- [DefinitionId] & [ObjectId]: Transform source document (Input, XML)
 	-- [TransformId] & [ObjectId]: Transform result (Output)
+	-- [TransformId]: Transform result (Output, fixed)
 	[DefinitionId]			UniqueIdentifier Null,
 	[TransformId]			UniqueIdentifier Null,
-	[ObjectId]				UniqueIdentifier Not Null,
+	[ObjectId]				UniqueIdentifier Null,
 	-- Temporal History Support
 	[SysStart] DATETIME2 (7) GENERATED ALWAYS AS ROW START HIDDEN NOT NULL CONSTRAINT [DF_TemplateDocument_SysStart] DEFAULT (sysdatetime()),
 	[SysEnd] DATETIME2 (7) GENERATED ALWAYS AS ROW END HIDDEN NOT NULL CONSTRAINT [DF_TemplateDocument_SysEnd] DEFAULT ('9999-12-31 23:59:59.9999999'),
@@ -18,7 +20,11 @@
 	CONSTRAINT [FK_TemplateDocumentObject] FOREIGN KEY ([TemplateId], [ObjectId]) REFERENCES [AppPOC].[TemplateObject] ([TemplateId], [ObjectId]),
 	CONSTRAINT [FK_TemplateDocumentTransform] FOREIGN KEY ([TemplateId], [TransformId]) REFERENCES [AppPOC].[TemplateTransform] ([TemplateId], [TransformId]),
 	CONSTRAINT [FK_TemplateDocumentDefinition] FOREIGN KEY ([TemplateId], [DefinitionId]) REFERENCES [AppPOC].[TemplateTransform] ([TemplateId], [DefinitionId]),
-	CONSTRAINT [CK_TemplateDocumentType] CHECK (([DefinitionId] is Not Null and [TransformId] is Null) Or ([DefinitionId] is Null and [TransformId] is Not Null)),
+	CONSTRAINT [CK_TemplateDocumentType] CHECK (
+	([DefinitionId] is Null and [TransformId] is Null And [ObjectId] is Null) Or -- Input, Fixed
+	([DefinitionId] is Not Null and [TransformId] is Null And [ObjectId] is Not Null) Or -- Input, object
+	([DefinitionId] is Null and [TransformId] is Not Null And [ObjectId] is Not Null) Or -- Output, object
+	([DefinitionId] is Null and [TransformId] is Not Null And [ObjectId] is Null)), -- Output, Fixed
 )
 GO
 CREATE INDEX [AK_TemplateDocumentDefinition] On [AppPOC].[TemplateDocument] ([TemplateId] ASC, [DefinitionId] ASC, [ObjectId] ASC) WHERE ([DefinitionId] is Not Null)
