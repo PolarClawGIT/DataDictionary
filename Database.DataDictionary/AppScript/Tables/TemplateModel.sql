d701f5ec160d0fae739bd01020e97c894d3f2fb8
PolarClawGit <PolarClawIce@Comcast.Net>
2026-03-18 07:58:02 -0700

    Fixed name of Default in the Template Model.

--- Database.DataDictionary/AppScript/Tables/TemplateModel.sql
+++ Database.DataDictionary/AppScript/Tables/TemplateModel.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [AppScript].[TemplateModel]
 (	-- Template(s) associated with a Model
-	[ModelId]		UNIQUEIDENTIFIER Not Null,
-	[TemplateId]	UNIQUEIDENTIFIER Not Null,
+	[ModelId]		UniqueIdentifier Not Null,
+	[TemplateId]	UniqueIdentifier Not Null,
 	-- Temporal History Support
 	[SysStart] DATETIME2 (7) GENERATED ALWAYS AS ROW START HIDDEN NOT NULL CONSTRAINT [DF_TemplateModel_SysStart] DEFAULT (sysdatetime()),
-	[SysEnd] DATETIME2 (7) GENERATED ALWAYS AS ROW END HIDDEN NOT NULL CONSTRAINT [TemplateModel_SysEnd] DEFAULT ('9999-12-31 23:59:59.9999999'),
+	[SysEnd] DATETIME2 (7) GENERATED ALWAYS AS ROW END HIDDEN NOT NULL CONSTRAINT [DF_TemplateModel_SysEnd] DEFAULT ('9999-12-31 23:59:59.9999999'),
    	PERIOD FOR SYSTEM_TIME ([SysStart], [SysEnd]),
 	-- Keys
 	CONSTRAINT [PK_TemplateModel] PRIMARY KEY CLUSTERED ([ModelId] ASC, [TemplateId] ASC),
