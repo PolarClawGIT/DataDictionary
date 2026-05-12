a0144b8a120789ad5e29c1d23a9bdcc642d67ad2
PolarClawGit <PolarClawIce@Comcast.Net>
2026-05-12 08:01:02 -0700

    Fix DataTable mix-match for Scripting Schema.

--- DataDictionary.DataLayer/AppScript/SchemaDefinition.Item.cs
+++ DataDictionary.DataLayer/AppScript/SchemaDefinition.Item.cs
@@ -175,8 +175,8 @@ namespace DataDictionary.DataLayer.AppScript
         static readonly IReadOnlyList<DataColumn> columnDefinitions =
         [
             new DataColumn(nameof(SchemaId), typeof(Guid)){ AllowDBNull = false},
-            new DataColumn(nameof(SchemaTitle), typeof(String)){ AllowDBNull = false},
             new DataColumn(nameof(TemplateId), typeof(Guid)){ AllowDBNull = true},
+            new DataColumn(nameof(SchemaTitle), typeof(String)){ AllowDBNull = false},
             new DataColumn(nameof(ForEachScope), typeof(String)){ AllowDBNull = true},
             new DataColumn(nameof(RootNodeName), typeof(String)){ AllowDBNull = true},
             new DataColumn(nameof(RootFolder), typeof(String)){ AllowDBNull = true},
