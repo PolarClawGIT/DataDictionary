ae52bcabd2e5d642449d6ccb22339d3df30ea963
PolarClawGit <PolarClawIce@Comcast.Net>
2026-07-29 09:34:20 -0700

    Updated Database to match Application.

--- Database.DataDictionary/HsScript/Tables/SchemaNode.sql
+++ Database.DataDictionary/HsScript/Tables/SchemaNode.sql
@@ -1,18 +1,18 @@
 ﻿CREATE TABLE [HsScript].[SchemaNode]
 (	[NodeId]				UniqueIdentifier Not Null,
 	[SchemaId]				UniqueIdentifier Not Null,
-	[NodeName]				[AppGeneral].[uddtMember] Null,
-	[NodeOrder]				Int Not Null,
-	[RenderValueAs]			NVarChar(20) Not Null,
-	[FixedValue]			NVarChar(250) Null,
-	[ObjectScope]			[AppGeneral].[uddtScopeName] Null,
+	[ObjectScope]			[AppGeneral].[uddtScopeName] Not Null,
 	[ObjectProperty]		[AppGeneral].[uddtQualifiedName] Null,
-	[PropertyId]			UniqueIdentifier NULL,
+	[NodeName]				[AppGeneral].[uddtMember] Null,
+	[RenderNodeType]		NVarChar(30) Null,
+	[RenderTypeCode]		NVarChar(30) Null,
+	[RenderOrder]			Int Not Null,
 	-- Temporal History Support
 	[SysStart]				DateTime2 (7) Not Null,
 	[SysEnd]				DateTime2 (7) Not Null,
 )
 GO
+
 CREATE CLUSTERED INDEX [IX_SchemaNode]
     ON [HsScript].[SchemaNode]([SysEnd] ASC, [SysStart] ASC)
 GO
